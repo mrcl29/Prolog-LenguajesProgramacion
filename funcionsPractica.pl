02b7eb0318af427ceb63bb015c934bd2c8033d8e
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-05-16 13:44:33 +0200

    Update

--- funcionsPractica.pl
+++ funcionsPractica.pl
@@ -15,6 +15,40 @@ escriuNonograma([X|L1]):-write(X),nl,escriuNonograma(L1).
 %Pintar_Pantalla
 mostraNonograma(Nono,Files,Columnes,IncFiles, IncColumnes).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Predicado auxiliar para imprimir espacios en blanco
+imprimir_espacios(0).
+imprimir_espacios(N) :-
+    write(' '),
+    N1 is N - 1,
+    imprimir_espacios(N1).
+
+% Predicado auxiliar para imprimir una fila de la lista
+imprimir_fila([]).
+imprimir_fila([X|Xs]) :-
+    write(X),
+    write(' | '),
+    imprimir_fila(Xs).
+
+% Predicado principal para imprimir la lista de listas
+imprimir_lista(Lista, NumColumnas, NumFilas, SeparacionColumnas, SeparacionFilas) :-
+    imprimir_filas(Lista, NumColumnas, NumFilas, SeparacionColumnas, SeparacionFilas, 1).
+
+% Predicado auxiliar para imprimir las filas de la lista
+imprimir_filas([], _, _, _, _, _).
+imprimir_filas([Fila|Filas], NumColumnas, NumFilas, SeparacionColumnas, SeparacionFilas, FilaActual) :-
+    imprimir_fila(Fila),
+    nl,
+    FilaActual < NumFilas,
+    imprimir_espacios(SeparacionFilas),
+    FilaActual1 is FilaActual + 1,
+    imprimir_filas(Filas, NumColumnas, NumFilas, SeparacionColumnas, SeparacionFilas, FilaActual1).
+imprimir_filas([_], NumColumnas, NumFilas, _, SeparacionFilas, FilaActual) :-
+    FilaActual =:= NumFilas.
+
+imprimir_lista(Lista, NumColumnas, NumFilas, SeparacionColumnas, SeparacionFilas).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Construir_Nonograma_Aleatori
 ferNonograma(Colors, Files, Columnes,Nono).
 
